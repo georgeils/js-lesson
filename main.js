5f9585268701dafed3981fc9f601d676b80e6f31
georgeils <v.vince@yandex.ru>
2020-12-24 19:17:10 +0300

    start movie app

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-document.write("Yes!")
+"use strict"
+
+const numberOfFilms = +prompt('How many films have you are seen?', '');
+
+const personalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+}
+
+const a = prompt('One of the last viewed movies?', ''),
+    b = prompt('How much do you rate?', ''),
+    c = prompt('One of the last viewed movies?', ''),
+    d = prompt('How much do you rate?', '');
+
+personalMovieDb.movies[a] = b;
+personalMovieDb.movies[c] = d;
+
+console.log(personalMovieDb);
